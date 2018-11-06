83a518726c0799b9c8819d322d03f448e5cf1ff1
alu0101070650 <alu0101070650@ull.edu.es>
2018-11-06 23:15:24 +0000

    Basic MERN app with users API

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import users from './routes/user.routes';
+
+// const fs = require('fs');
+
+const app = express();
+dotenv.config();
+
+app.set('port', process.env.PORT || 3001);
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// Set native promises as mongoose promise
+mongoose.Promise = global.Promise;
+
+// MongoDB Connection
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/videocon').catch((error) => {
+    if (error) {
+      console.error('Cannot connect to MongoDB!'); // eslint-disable-line no-console
+      // throw error;
+    }
+  });
+}
+
+app.use('/api', users);
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});

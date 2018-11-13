107ed255c15f0e0a77c9b140c6d1f013fa7382da
alu0100974331 <32130179+alu0100974331@users.noreply.github.com>
2018-11-13 22:59:36 +0000

    LandingPage acabado con Semantic

---
+++ client/src/modules/RoomName/RoomName.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Input } from 'semantic-ui-react'
+
+    const InputExampleFluid = () => (<Input fluid icon='users' iconPosition='left' placeholder='Introduce name for your room...' />)
+
+export default InputExampleFluid

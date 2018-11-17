998e1201d3fbcd9c6260842a7d8e27f726224ff7
carlos Arvelo Garcia <alu0100943849@ull.edu.es>
2018-11-17 13:50:40 +0000

    added BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './semantic/dist/semantic.min.css';
-import LandingPage from './modules/LandingPage/LandingPage';
-// import * as serviceWorker from './serviceWorker';
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import { BrowserRouter } from 'react-router-dom'
+ import './index.css';
+ import './semantic/dist/semantic.min.css';
+ // import LandingPage from './modules/LandingPage/LandingPage';
+ import ReactRouter from './modules/reactRouterExample/ReactRouter';
+ // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
-
+// ReactDOM.render(<LandingPage />, document.getElementById('root'));
+ ReactDOM.render(
+    <BrowserRouter>
+        <ReactRouter />
+    </BrowserRouter>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
---
+++ client/src/modules/reactRouterExample/ReactRouter.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch  } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+import SemanticUIExample from '../SemanticUIExample/SemanticUIExample.js'
+// import logo from './logo.svg';
+import './LandingPage.css';
+import Pagina1 from './Pagina1';
+import Pagina2 from './Pagina2';
+
+class ReactRouter extends Component { //class LandingPage extends Component {
+  render() {
+    return (
+      <div className ='LandingPage'>
+        <header className='LandingPage-header'>
+          <nav>
+            <ul>
+              <li><Link to='/'>ReactRouter</Link></li>
+              <li><Link to='/pagina1'>Pagina1</Link></li>
+              <li><Link to='/pagina2'>Pagina2</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/pagina1' component={Pagina1} />
+            <Route path='/pagina2' component={Pagina2} />
+          </Switch>
+          <Route exact={true} path="/" render={() => ( //
+            <div>
+              <p>
+                Videocon.io landing page (WIP)
+              </p>
+              <a
+                className='LandingPage-link'
+                href='https://github.com/SyTW12018/E01-videocon'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                GitHub
+              </a>
+              <Container className='LandingPage-semantic-example'>
+                <SemanticUIExample />
+              </Container>
+            </div>
+          )}/>
+        </header>
+        </div>
+    );
+  }
+}
+
+export default ReactRouter;
+

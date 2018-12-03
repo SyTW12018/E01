a5372c71b9c91383e738ffa8629074138e224f93
Joram Real Gómez <joramreal@gmail.com>
2018-12-03 20:55:54 +0000

    Ejemplos de chat y render básico de ChatSection

--- client/src/modules/ComponenteChats/components/ChatSection/ChatSection.js
+++ client/src/modules/ComponenteChats/components/ChatSection/ChatSection.js
@@ -7,7 +7,39 @@ class ChatSection extends React.Component {
         super(props);
 
         this.state = {
-            chats: [{}]
+            chats: [{
+                username: "Derecha",
+                content: <p>Hello World!</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Izquierda",
+                content: <p>Prueba de chat</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Derecha",
+                content: <p>Mensaje recibido gracias por todo</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Izquierda",
+                content: <p>Lorem ipsum dolor sit amet, nibh ipsum. Cum class sem inceptos incidunt sed sed. Tempus wisi enim id, arcu sed lectus aliquam, nulla vitae est bibendum molestie elit risus.</p>,
+                img: "http://i.imgur.com/ARbQZix.jpg",
+            }, {
+                username: "Derecha",
+                content: <p>Esto es un placeholder como una casa</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Derecha",
+                content: <p>Videocon es una aplicacion de videoconferencia para hablar con tus amigos</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Derecha",
+                content: <p>Conectate ahora con nostros!</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }, {
+                username: "Izquierda",
+                content: <p>Magnifica Aplicación!</p>,
+                img: "http://i.imgur.com/Tj5DGiO.jpg",
+            }]
         };
 
         this.submitMessage = this.submitMessage.bind(this);
@@ -37,6 +69,17 @@ class ChatSection extends React.Component {
         }, () => {
             ReactDOM.findDOMNode(this.refs.msg).value = "";
         });
+    }
+
+    render() {
+        const username = "Derecha";
+        const { chats } = this.state;
+
+        return (
+            <div className="Chatroom">
+                <h3>Chat</h3>
+            </div>
+        );
     }
 }
 

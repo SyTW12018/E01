50795ca7ebd0ec87f9d5628d044b400e7d68dff7
alu0101070650 <alu0101070650@ull.edu.es>
2018-12-30 14:26:49 +0000

    Refactored RoomNameInput

--- client/src/modules/RoomNameInput/RoomNameInput.js
+++ client/src/modules/RoomNameInput/RoomNameInput.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Form } from 'formsy-semantic-ui-react';
-import { Message, Grid } from 'semantic-ui-react';
-import PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
+import removeAccents from 'remove-accents';
+import { Redirect } from 'react-router';
+import { StripChar } from 'stripchar';
 import styles from './RoomNameInput.css';
-import removeAccents from  'remove-accents'
-import { Redirect } from 'react-router'
-import { StripChar } from 'stripchar'
 
 export default class RoomNameInput extends Component {
-  static propTypes = {
-    refreshAuth: PropTypes.func.isRequired,
-  };
-
   constructor(props) {
     super(props);
 
@@ -24,30 +19,30 @@ export default class RoomNameInput extends Component {
     };
 
     this.form = null;
-    this.refreshAuth = props.refreshAuth;
-  }
-  
-  formatName = (str) => {
-    str = str.toLowerCase();
-    str = str.replace(/ /g,"_");
-    str = removeAccents(str);
-    str = StripChar.RSExceptUnsAlpNum(str, '_');
-    return str;
   }
 
-  handleChange = (e, { name, value }) => {
-    let formatedNameRoom = this.formatName(value); 
-    this.setState({ roomName: formatedNameRoom })
-  }
-  
-  register = async (formData) => {
+  formatName = (str) => {
+    let name = str;
+    name = name.toLowerCase();
+    name = name.replace(/ /g, '_');
+    name = removeAccents(name);
+    name = StripChar.RSExceptUnsAlpNum(name, '_');
+    return name;
+  };
+
+  handleChange = (e, { value }) => {
+    const formattedNameRoom = this.formatName(value);
+    this.setState({ roomName: formattedNameRoom });
+  };
+
+  joinRoom = async (formData) => {
     this.setState({ isLoading: true });
 
     let errors = [];
     try {
       const result = await axios.post(`/rooms/${formData.roomName}`);
       if (result.status === 201 || result.status === 200) {
-          this.setState({ redirect: true })
+        this.setState({ redirect: true });
         return;
       }
     } catch (e) {
@@ -70,57 +65,47 @@ export default class RoomNameInput extends Component {
 
   render() {
     const {
-      roomName, isLoading, errors,
+      roomName, isLoading, errors, redirect,
     } = this.state;
 
-    if(this.state.redirect){
+    if (redirect) {
       return (
-        <Redirect to={`/room/${this.state.roomName}`} />
-      )
-    } else {
-
-      return (
-
-        <Grid verticalAlign='middle'>
-          <Grid.Column>
-            <Form
-              warning
-              loading={isLoading}
-              onValidSubmit={this.register}
-              onValid={() => this.setState({ isValid: true })}
-              onInvalid={() => this.setState({ isValid: false })}
-              ref={(ref) => { this.form = ref; }}
-            >
-              <Form.Input
-                className='hiddenLabel'
-                name='roomName'
-                size='huge'
-                icon='video camera'
-                iconPosition='left'
-                placeholder='Room Name'
-                value={roomName}
-                action={{ icon: 'video camera', color: 'orange', className: styles.roomNameInputButton }}
-                onChange={this.handleChange}
-                errorLabel={<Message warning />}
-                validationErrors={{
-                  isAlphanumeric: 'Enter a valid Room Name, without special characters, accent and spaces',
-                  isDefaultRequiredValue: 'Room Name is required',
-
-                }}
-              />
-
-              { errors.length > 0 ? (
-                <Message
-                  negative
-                  header="Can't create a new room!"
-                  list={errors}
-                />
-              ) : (null)}
-
-            </Form>
-          </Grid.Column>
-        </Grid>
+        <Redirect to={`/room/${roomName}`} />
       );
     }
+
+    return (
+      <Form
+        warning
+        loading={isLoading}
+        onValidSubmit={this.joinRoom}
+        ref={(ref) => { this.form = ref; }}
+      >
+        <Form.Input
+          placeholder='Name of the room'
+          action={{ icon: 'video camera', color: 'orange', className: styles.roomNameInputButton }}
+          className={styles.roomNameInput}
+          size='huge'
+          color='orange'
+          name='roomName'
+          value={roomName}
+          onChange={this.handleChange}
+          errorLabel={<Message warning />}
+          validationErrors={{
+            isAlphanumeric: 'Enter a valid Room Name, without special characters, accent and spaces',
+            isDefaultRequiredValue: 'Room Name is required',
+          }}
+        />
+
+        { errors.length > 0 ? (
+          <Message
+            negative
+            header="Can't create a new room!"
+            list={errors}
+          />
+        ) : (null)}
+
+      </Form>
+    );
   }
 }

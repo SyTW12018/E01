bbc89d5bac64da49e96527c8ab1e17a8528a310d
carlos Arvelo Garcia <alu0100943849@ull.edu.es>
2019-01-11 11:42:48 +0000

    User data displayed in user settings

--- client/src/modules/NavBar/components/SettingsForm/SettingsForm.js
+++ client/src/modules/NavBar/components/SettingsForm/SettingsForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Form } from 'formsy-semantic-ui-react';
+import { AuthConsumer } from 'react-check-auth';
 import {
-  Modal, Button, Message, Grid, Header, Icon, Container, Divider, Dropdown,
+  Modal, Button, Grid, Header, Icon, Container, Dropdown, List,
 } from 'semantic-ui-react';
 import '../Form.css';
 import PropTypes from 'prop-types';
@@ -60,9 +60,56 @@ export default class SettingsForm extends Component {
     });
   };
 
+  UserInfo = userInfo => (
+    <Container fluid>
+      <Header>
+        <List divided verticalAlign='middle'>
+          <List.Item>
+            <List.Content floated='right'>
+              <Button color='orange'>
+                <Icon size='small' name='pencil' />
+                edit
+              </Button>
+            </List.Content>
+            <Icon size='large' name='user' />
+            <List.Content>{` ${userInfo.name}`}</List.Content>
+          </List.Item>
+
+          <List.Item>
+            <List.Content floated='right'>
+              <Button color='orange'>
+                <Icon size='small' name='pencil' />
+                edit
+              </Button>
+            </List.Content>
+            <Icon size='large' name='mail' />
+            <List.Content>{` ${userInfo.email}`}</List.Content>
+          </List.Item>
+      
+          <List.Item>
+            <List.Content floated='right'>
+              <Button color='orange'>
+                <Icon size='small' name='pencil' />
+                edit
+              </Button>
+            </List.Content>
+            <Icon size='large' name='lock' />
+            <List.Content>Password</List.Content>
+          </List.Item>
+        </List>
+      </Header>
+      <Header>
+        <Button color='orange' fluid>
+          <Icon size='large' name='trash alternate' />
+            Delete Account
+          </Button>
+      </Header>
+    </Container>
+  );
+
   render() {
     const {
-      isModalOpen, isValid, isLoading, errors,
+      isModalOpen,
     } = this.state;
 
 
@@ -82,113 +129,9 @@ export default class SettingsForm extends Component {
                 <Icon name='weixin' />
                 My Profile
               </Header>
-
-              <Divider />
-
-              <Form
-                warning
-                size='large'
-                loading={isLoading}
-                // /siguiente línea a meditarla probablemente cambiar .register a .update
-                onValidSubmit={this.update}
-                onValid={() => this.setState({ isValid: true })}
-                onInvalid={() => this.setState({ isValid: false })}
-                ref={(ref) => { this.form = ref; }}
-              >
-                <Form.Input
-                  disabled
-                  className='hiddenLabel'
-                  name='email'
-                  fluid
-                  required
-                  icon='at'
-                  iconPosition='left'
-                  placeholder='Aquí iría el email recibido de algún lado'
-
-                />
-
-                <Form.Input
-                  className='hiddenLabel'
-                  name='username'
-                  fluid
-                  required
-                  icon='user'
-                  iconPosition='left'
-                  placeholder='Username'
-                  validations={{
-                    isAlphanumeric: true,
-                    minLength: 3,
-                    maxLength: 25,
-                  }}
-                  errorLabel={<Message warning />}
-                  validationErrors={{
-                    isAlphanumeric: 'Enter a valid username',
-                    minLength: 'The username must have a length between 3 and 25',
-                    maxLength: 'The username must have a length between 3 and 25',
-                    isDefaultRequiredValue: 'Username is required',
-                  }}
-                />
-
-                <Form.Input
-                  className='hiddenLabel'
-                  name='password'
-                  fluid
-                  required
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password'
-                  type='password'
-                  validations={{
-                    minLength: 5,
-                    maxLength: 40,
-                  }}
-                  errorLabel={<Message warning />}
-                  validationErrors={{
-                    minLength: 'The password must have a length between 5 and 40',
-                    maxLength: 'The password must have a length between 5 and 40',
-                    isDefaultRequiredValue: 'Password is required',
-                  }}
-                />
-
-                <Form.Input
-                  className='hiddenLabel'
-                  name='passwordRepeat'
-                  fluid
-                  required
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Repeat password'
-                  type='password'
-                  validations='equalsField:password'
-                  errorLabel={<Message warning />}
-                  validationErrors={{
-                    isDefaultRequiredValue: 'Password is required',
-                    equalsField: 'Passwords doesn\'t match',
-                  }}
-                />
-
-                { errors.length > 0 ? (
-                  <Message
-                    negative
-                    header="Can't create a new account!"
-                    list={errors}
-                  />
-                ) : (null)}
-
-                <Divider />
-
-                <Container fluid>
-                  <Button
-                    fluid
-                    size='large'
-                    color='orange'
-                    content='Register'
-                    disabled={!isValid || isLoading}
-                  />
-                </Container>
-
-              </Form>
-
+                <AuthConsumer>
+                  {({ userInfo }) => (userInfo ? this.UserInfo(userInfo) : null)}
+                </AuthConsumer>
             </Grid.Column>
           </Grid>
         </Modal.Content>
